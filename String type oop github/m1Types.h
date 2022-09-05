c40dd450fcee614b9991b8a64dff926f5cb17ea6
Zp <62895009+PPN123456@users.noreply.github.com>
2022-09-06 00:17:03 +0200

    Update m1Types.h

--- String type oop github/m1Types.h
+++ String type oop github/m1Types.h
@@ -16,15 +16,15 @@ PUBLIC m1String*     m1String_newReadFromFile ( const char *fileName);
 PUBLIC void 	     m1String_free	    	  ( m1String** st);
 
 // SET
-PUBLIC void       m1String_setValue  ( m1String  *st, const char *str);
+PUBLIC void         m1String_setValue  ( m1String  *st, const char *str);
 //GET
 PUBLIC ULLONG        m1String_getLength  ( m1String *st);
 PUBLIC const char*   m1String_getValue   ( m1String *st);
 PUBLIC char*         m1String_getSlice   ( m1String *st,  int begin, int end);
 
 PUBLIC void          m1String_saveToFile   ( m1String *st, const char *fileName);
-PUBLIC void			 m1String_toUpper      ( m1String *st);
-PUBLIC void			 m1String_toLower  	   ( m1String *st);
+PUBLIC void          m1String_toUpper      ( m1String *st);
+PUBLIC void          m1String_toLower  	   ( m1String *st);
 PUBLIC void          m1String_mod          ( m1String *st, char mod, const char* str);
 
 #endif
